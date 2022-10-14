4b64206064619b93fba3b0bdb1912768c07ffcea
Ali Hamza <miangdpp@gmail.com>
2022-10-14 21:24:35 +0500

    version 1
    add custom link to opencart main menu

---
+++ admin/language/en-gb/extension/module/favourite_link.php
@@ -0,0 +1,17 @@
+<?php
+// Heading
+$_['heading_title']    = 'Favourite Link';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified favourite link module!';
+$_['text_edit']        = 'Edit Favourite Link Module';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order']     = 'Sort Order';
+$_['entry_link']     = 'Link';
+$_['entry_title']     = 'Title';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Favourite link module!';
